9a49690f6ce315c51cca0a5ba2dc76f0fc9832ae
snehalkorade9 <snehal.korade99@gmail.com>
2022-03-07 23:50:24 +0530

    Updated yml files to deploy and testing files

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -7,7 +7,7 @@ def test_request_main_menu_links(client):
     assert response.status_code == 200
     #assert b'<a class="nav-link" href="/about1.html">About</a>' in response.data
     #assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
+    assert b'<a class="nav-link" href="/Git">Git</a>' in response.data
     #assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
     #assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
 
@@ -16,30 +16,24 @@ def test_request_index_sk(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<h5 class="card-title">Card 1</h5>' in response.data
-    assert b'<h5 class="card-title">Card 2</h5>' in response.data
+    assert b'<h3><a href="/Docker">Docker</a></h3>' in response.data
 
 
-def test_request_about(client):
+def test_request_git(client):
     """This makes the index page"""
-    response = client.get("/about1")
+    response = client.get("/Git")
     assert response.status_code == 200
     #assert b"About Page" in response.data1234
-    assert b'<li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>' in response.data
+    assert b'<h4>Git is Version control system, allowing you to work together and collabrate with ' in response.data
 
 
-def test_request_page1(client):
+def test_request_docker(client):
     """This makes the index page"""
-    response = client.get("/page1")
+    response = client.get("/Docker")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b" Pushes an image or a repository to a registry" in response.data
 
 
-def test_request_page2(client):
-    """This makes the index page"""
-    response = client.get("/page2")
-    assert response.status_code == 200
-    assert b"Page 2" in response.data
 
 
 def test_request_page3(client):
@@ -60,4 +54,3 @@ def test_request_page_not_found(client):
     """This makes the index page"""
     response = client.get("/page5")
     assert response.status_code == 404
-
